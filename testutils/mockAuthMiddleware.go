1c15f7944ff4d92d49a4752b69dae6fb5d9433f7
Mathilde Hagl <mathilde.s.hagl@tum.de>
2026-03-07 16:25:16 +0100

    refactor: remove redundant comments in MockAuthMiddlewareWithEmail

--- testutils/mockAuthMiddleware.go
+++ testutils/mockAuthMiddleware.go
@@ -7,8 +7,6 @@ import (
 
 func MockAuthMiddlewareWithEmail(mockRoles []string, email, matriculationNumber, universityLogin string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Optionally set mock data, such as roles, for downstream handlers
-		// Create a map for user roles
 		userRoles := make(map[string]bool)
 		for _, role := range mockRoles {
 			userRoles[role] = true
